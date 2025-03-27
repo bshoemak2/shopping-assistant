fe7ab6dab4b603cccd94481f0c46e4e4e5a308fe
bshoemak2 <bshoemak@mac.com>
2025-03-27 10:37:48 -0400

    Use environment variables for email credentials

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ app.static_folder = 'static'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'bshoemak2@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ycvp qmmg rbcl kdfg'
-app.config['MAIL_DEFAULT_SENDER'] = 'bshoemak2@gmail.com'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'bshoemak2@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'ycvp qmmg rbcl kdfg')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME', 'bshoemak2@gmail.com')
 
 mail = Mail(app)
 
